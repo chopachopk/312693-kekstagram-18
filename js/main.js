91389414831df37f08e4e131b980960f65729e07
chopachopk <monkeyslutz@yandex.ru>
2019-10-31 00:41:36 +0300

    Реализовала применение эффекта для изображения

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ var NUMBER_OF_AVATARS = 6;
 var COMMENT_MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var COMMENTATORS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
+// Заполнение фото от других пользователей
 var photosListElement = document.querySelector('.pictures');
 
 var pictureTemplate = document.querySelector('#picture')
@@ -61,9 +62,11 @@ for (var i = 0; i < photos.length; i++) {
 }
 photosListElement.appendChild(fragment);
 
+// Открытие и закрытие попапа с загруженным фото
 var uploadInput = document.querySelector('.img-upload__input');
 var upload = document.querySelector('.img-upload__overlay');
 var uploadCancel = upload.querySelector('.img-upload__cancel');
+var imagePreview = document.querySelector('.img-upload__preview > img');
 
 var onPopupEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
@@ -73,6 +76,8 @@ var onPopupEscPress = function (evt) {
 
 var openPopup = function () {
   upload.classList.remove('hidden');
+  imagePreview.style = 'filter: none';
+  effectLevel.classList.add('hidden');
   document.addEventListener('keydown', onPopupEscPress);
 };
 
@@ -96,6 +101,7 @@ uploadCancel.addEventListener('keydown', function (evt) {
   }
 });
 
+// Масштабирование изображения
 var MIN_SCALE = 25;
 var MAX_SCALE = 100;
 var STEP_SCALE = 25;
@@ -103,7 +109,6 @@ var smallerButton = document.querySelector('.scale__control--smaller');
 var biggerButton = document.querySelector('.scale__control--bigger');
 var scaleValue = document.querySelector('.scale__control--value');
 var scaleValueInt = parseInt(scaleValue.value, 10);
-var imagePreview = document.querySelector('.img-upload__preview img');
 
 var scaleImage = function (evt) {
   evt.preventDefault();
@@ -117,3 +122,87 @@ var scaleImage = function (evt) {
 };
 smallerButton.addEventListener('click', scaleImage);
 biggerButton.addEventListener('click', scaleImage);
+
+// Применение фильтров к изображению
+var effects = document.querySelector('.img-upload__effects');
+var effectLevel = document.querySelector('.effect-level');
+var effectLevelValue = document.querySelector('.effect-level__value');
+var effectLevelLine = document.querySelector('.effect-level__line');
+var effectLevelPin = document.querySelector('.effect-level__pin');
+var effectLevelDepth = document.querySelector('.effect-level__depth');
+
+effectLevel.classList.add('hidden');
+
+var setEffectLevel = function (effect, level) {
+  switch (effect.value) {
+    case 'none':
+      return '';
+    case 'chrome':
+      return 'filter: grayscale(' + level + ');';
+    case 'sepia':
+      return 'filter: sepia(' + level + ');';
+    case 'marvin':
+      return 'filter: invert(' + level * 100 + '%);';
+    case 'phobos':
+      return 'filter: blur(' + level * 3 + 'px);';
+    case 'heat':
+      return 'filter: brightness(' + (level * 2 + 1) + ');';
+    default:
+      return '';
+  }
+};
+
+effects.addEventListener('change', function (evt) {
+  evt.preventDefault();
+
+  if (evt.target.value === 'none') {
+    effectLevel.classList.add('hidden');
+  } else {
+    effectLevel.classList.remove('hidden');
+  }
+  imagePreview.classList = 'effects__preview--' + evt.target.value;
+  imagePreview.style = setEffectLevel(evt.target, 1);
+  effectLevelPin.style.left = '100%';
+  effectLevelDepth.style.width = '100%';
+});
+
+effectLevelPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var scaleBlock = effectLevelLine.getBoundingClientRect();
+  var chosenEffect = document.querySelector('.effects__radio:checked');
+  var pinCoordX = evt.clientX;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shift = pinCoordX - moveEvt.clientX;
+    pinCoordX = moveEvt.clientX;
+
+    if (moveEvt.clientX <= scaleBlock.left) {
+      effectLevelPin.style.left = '0%';
+      effectLevelDepth.style.width = '0%';
+    }
+    if (moveEvt.clientX >= scaleBlock.right) {
+      effectLevelPin.style.left = '100%';
+      effectLevelDepth.style.width = '100%';
+    } else {
+      effectLevelPin.style.left = (effectLevelPin.offsetLeft - shift) + 'px';
+      effectLevelDepth.style.width = (effectLevelPin.offsetLeft - shift) / (scaleBlock.width) * 100 + '%';
+    }
+    var ratio = (pinCoordX - scaleBlock.left) / scaleBlock.width;
+    if (ratio < 0) {
+      ratio = 0;
+    }
+    effectLevelValue.value = Math.round(ratio * 100);
+    imagePreview.style = setEffectLevel(chosenEffect, ratio);
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
